9d55afce87cebb2d9e6c8b90e67bb15fc72b5138
toran billups <toranb@gmail.com>
2014-05-27 19:48:18 -0500

    added gulp-filter (mostly working gulp tasks)

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,6 +3,7 @@ var gutil = require('gulp-util');
 var debug = require('gulp-debug');
 var karma = require('gulp-karma');
 var concat = require('gulp-concat');
+var gulpFilter = require('gulp-filter');
 var handlebars = require('gulp-ember-handlebars');
 var transpiler = require('gulp-es6-module-transpiler');
 
@@ -10,69 +11,50 @@ var paths = {
     templates: [
         'js/templates/**/*.handlebars'
     ],
-    scripts: [
-        'js/app/**/*.js'
-    ],
-    tests: [
-        'js/tests/**/*.js'
-    ],
     concatDist: [
         'js/vendor/jquery/jquery.min.js',
         'js/vendor/handlebars/handlebars.js',
-        'js/vendor/ember/ember.js',
-        'js/dist/tmpl.min.js',
-        'vendor/loader.js',
+        'js/vendor/ember/ember.min.js',
+        'js/vendor/ember-loader/loader.js',
         'vendor/ember-resolver.js',
-        'js/dist/transpiled/app/**/*.js'
+        'js/dist/tmpl.min.js',
+        'js/app/**/*.js'
     ],
     concatTest: [
         'js/vendor/jquery/jquery.min.js',
         'js/vendor/handlebars/handlebars.js',
         'js/vendor/ember/ember.js',
-        'js/vendor/jquery-mockjax/jquery.mockjax.js',
-        'js/dist/tmpl.min.js',
-        'vendor/loader.js',
+        'js/vendor/ember-loader/loader.js',
         'vendor/ember-resolver.js',
-        'js/dist/transpiled/app/**/*.js',
-        'js/dist/transpiled/tests/**/*.js',
-        'vendor/test-loader.js'
+        'js/dist/tmpl.min.js',
+        'js/app/**/*.js',
+        'js/tests/**/*.js'
     ]
 };
 
+var filter = gulpFilter(function(file) {
+  return file.path.indexOf('vendor') === -1;
+});
+
 gulp.task('concat:dist', function(){
     return gulp.src(paths.concatDist)
+        .pipe(filter)
+        .pipe(transpiler({
+            type: "amd",
+        }))
+        .pipe(filter.restore())
         .pipe(concat('deps.min.js'))
         .pipe(gulp.dest('js/dist/'));
 });
 
 gulp.task('concat:test', function(){
     return gulp.src(paths.concatTest)
+        .pipe(filter)
+        .pipe(transpiler({
+            type: "amd",
+        }))
+        .pipe(filter.restore())
         .pipe(concat('deps.min.js'))
-        .pipe(gulp.dest('js/dist/'));
-});
-
-gulp.task('transpile:app', function(){
-    return gulp.src(paths.scripts)
-        .pipe(transpiler({
-            type: "amd",
-            moduleName: "js/"
-        }))
-        .pipe(gulp.dest('js/dist/transpiled/app/'));
-});
-
-gulp.task('transpile:test', function(){
-    return gulp.src(paths.tests)
-        .pipe(transpiler({
-            type: "amd",
-            moduleName: "js/tests/"
-        }))
-        .pipe(gulp.dest('js/dist/transpiled/tests/'));
-});
-
-gulp.task('emberhandlebars', function(){
-    return gulp.src(paths.templates)
-        .pipe(handlebars({outputType: 'browser'}))
-        .pipe(concat('tmpl.min.js'))
         .pipe(gulp.dest('js/dist/'));
 });
 
@@ -84,5 +66,12 @@ gulp.task('karma', function(){
         }));
 });
 
-gulp.task('local', ['transpile:app', 'emberhandlebars', 'concat:dist'])
-gulp.task('test', ['transpile:app', 'transpile:test', 'emberhandlebars', 'concat:test', 'karma'])
+gulp.task('emberhandlebars', function(){
+    return gulp.src(paths.templates)
+        .pipe(handlebars({outputType: 'browser'}))
+        .pipe(concat('tmpl.min.js'))
+        .pipe(gulp.dest('js/dist/'));
+});
+
+gulp.task('default', ['emberhandlebars', 'concat:dist'])
+gulp.task('test', ['emberhandlebars', 'concat:test', 'karma'])
