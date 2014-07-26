4128367a91fb57f034471ca2e3d482ff01f9a523
toran billups <toranb@gmail.com>
2014-07-25 19:29:00 -0500

    added code coverage with karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,17 +1,29 @@
 module.exports = function(karma) {
     karma.set({
-        basePath: 'js',
+        basePath: '',
         files: [
-          "dist/deps.min.js"
+          "js/dist/deps.min.js"
         ],
         port: 9876,
         runnerPort: 9100,
         colors: true,
-        reporters: ['progress'],
+        reporters: ['progress', 'coverage'],
         logLevel: karma.LOG_ERROR,
         browsers: ['PhantomJS'],
         singleRun: true,
         autoWatch: false,
-        frameworks: ['qunit', 'qunit-sb'],
+        frameworks: ['qunit'],
+        preprocessors:{
+            "js/dist/deps.min.js": "coverage"
+        },
+        coverageReporter: {
+            type: "text",
+            dir: "coverage/"
+        },
+        plugins: [
+            "karma-coverage",
+            "karma-qunit",
+            "karma-phantomjs-launcher"
+        ]
     });
 };
